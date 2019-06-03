1f825b1303d1e7d0937fb9f6c4f9abd81b10565c
Hannah Simurdak <simurdh@wwu.edu>
2019-06-03 15:29:58 -0700

    fixed price adjustment

--- SimurdakAssignment3.java
+++ SimurdakAssignment3.java
@@ -50,22 +50,65 @@ class SimurdakAssignment3 {
 							" order by TickerCnt DESC, Industry");
 			
 			PreparedStatement industryTickers = conn
-					.prepareStatement("select Ticker, min(TransDate), max(TransDate), count(distinct TransDate)" + 
+					.prepareStatement("select Ticker, min(TransDate), max(TransDate), count(distinct TransDate) as TradingDays" + 
 							" from Company natural left outer join PriceVolume" + 
 							" where Industry = ? " + 
+							" and TransDate >= ? and TransDate <= ? " +
 							" group by Ticker" + 
+							" having TradingDays >= 150" +
 							" order by Ticker");
+			
+			PreparedStatement max_min_TransDate = conn
+					.prepareStatement("select max(minDates) from" + 
+							"(select min(TransDate) as minDates" + 
+							"							from Company natural left outer join PriceVolume" + 
+							"							where Industry = ? " + 
+							"							group by Ticker" + 
+							"							order by Ticker) as minDates");
+			
+			PreparedStatement min_max_TransDate = conn
+					.prepareStatement("select min(maxDates) from" + 
+							"(select max(TransDate) as maxDates" + 
+							"							from Company natural left outer join PriceVolume" + 
+							"							where Industry = ? " + 
+							"							group by Ticker" + 
+							"							order by Ticker) as maxDates");
 			
 			ResultSet rs = industry.executeQuery();
 			
 			while (rs.next()) { 
+				
+				
+				//Find max(min(TransDate)
+				max_min_TransDate.setString(1, rs.getString(1));
+				ResultSet result = max_min_TransDate.executeQuery();
+				String max_minDate = null;
+				if (result.next()) {
+					max_minDate = result.getString(1);
+				} 
+				
+				//Find min(max(TransDate)
+				min_max_TransDate.setString(1, rs.getString(1));
+				ResultSet result2 = min_max_TransDate.executeQuery();
+				String min_maxDate = null;
+				if (result2.next()) {
+					min_maxDate = result.getString(1);
+				}
+				
 				industryTickers.setString(1, rs.getString(1));
+				industryTickers.setString(2, max_minDate);
+				industryTickers.setString(3, min_maxDate);
 				ResultSet rs2 = industryTickers.executeQuery();
 				System.out.println("Industry: " + rs.getString(1) + " " + rs.getDouble(2));
 
 				while (rs2.next()) { //for each Industry, adjust prices for splits
-					showTicker(rs2.getString(1));
-					CalculatePrices();
+////					showTicker(rs2.getString(1));
+//					if (rs2.getString(1).equals("INTC")) {
+//						showTickerDays(rs2.getString(1), "1980.01.01", "1999.12.31");
+//						CalculatePrices();
+//					}
+////					CalculatePrices();
+					
 				}
 			}
 			
@@ -122,58 +165,94 @@ class SimurdakAssignment3 {
 		return 0;
 	}
 	
+	/* When dates are specified */
+	static int showTickerDays(String ticker, String start_date, String end_date) throws SQLException {
+
+		// Prepare query
+		PreparedStatement name = conn
+				.prepareStatement("select name, Ticker" + "	from company " + "   where Ticker = ?");
+
+		PreparedStatement PVdata1 = conn.prepareStatement("select *" + "	from PriceVolume "
+				+ "   where Ticker = ? and TransDate between ? and ? " + "   order by TransDate DESC");
+
+		// PV Data Columns: 1) Ticker 2) TransDate 3) OpenPrice 4)HighPrice 5) LowPrice
+		// 6) ClosePrice 7) Volume
+
+		name.setString(1, ticker);
+		ResultSet rs = name.executeQuery();
+
+		PVdata1.setString(1, ticker);
+		PVdata1.setString(1, ticker);
+		PVdata1.setString(2, start_date);
+		PVdata1.setString(3, end_date);
+
+		ResultSet rs2 = PVdata1.executeQuery();
+
+		if (rs.next()) {
+			System.out.printf("%s%n", rs.getString(1));
+			co_data = new CompanyData(rs.getString(1));
+			dayPrices = new ArrayList<Prices>();
+			while (rs2.next()) {
+				Prices curDay = new Prices(rs2.getString(2));
+				curDay.addDay(rs2.getDouble(3), rs2.getDouble(4), rs2.getDouble(5), rs2.getDouble(6));
+				dayPrices.add(curDay);
+				co_data.countDay();
+			}
+
+		} else {
+			System.out.printf("%s not found in database.\n%n", ticker);
+			return -1;
+
+		}
+		name.close();
+		PVdata1.close();
+		return 0;
+	}
+	
 	/* Calculate Price data */
 	static void CalculatePrices() {
 		int daySize = dayPrices.size() - 1;
 		String split = null;
-		int totalDivisor = 1;
-
-		// System.out.println("size: " + daySize);
+		double totalDivisor = 1.0;
 
 		for (int d = 0; d < daySize - 1; d++) {
 
-			// System.out.println(dayPrices.get(d).getDate() + " open: " +
-			// dayPrices.get(d).getOpen() + " close: " + dayPrices.get(d).getClose());
 			if (d > 0) {
 				split = calcSplitDay(dayPrices.get(d).getClose(), dayPrices.get(d - 1).getOpen());
 				if (split != null) {
-					if (split == "2:1") {
+					if (split.equals("2:1")) {
 						totalDivisor *= 2.0;
-					} else if (split == "3:1") {
+					} else if (split.equals("3:1")) {
 						totalDivisor *= 3.0;
-					} else if (split == "3:2") {
+					} else if (split.equals("3:2")) {
 						totalDivisor *= 1.5;
 					}
 					co_data.addSplitDay(dayPrices.get(d).getDate() + "\t" + dayPrices.get(d).getClose() + " --> "
 							+ dayPrices.get(d - 1).getOpen(), split);
 				}
-				dayPrices.get(d - 1).updatePrices(totalDivisor);
+
+				dayPrices.get(d - 1).updateClose(totalDivisor);
+				dayPrices.get(d - 1).updateOpen(totalDivisor);
+
 			}
 		}
-//		System.out.println("total div: " + totalDivisor);
-//		co_data.getSplitDays();
-//		 System.out.println("total div: " + totalDivisor);
-//		 for (int d = 0; d < daySize - 1; d++) {
-//		
-//		 System.out.println(dayPrices.get(d).getDate() + " open: " +
-//		 dayPrices.get(d).getOpen() + " close: " + dayPrices.get(d).getClose());
-//		
-//		 }
+		co_data.getSplitDays();
+		 System.out.println("total div: " + totalDivisor);
+		 for (int d = 0; d < daySize - 1; d++) {
+		
+		 System.out.println(dayPrices.get(d).getDate() + " open: " +
+		 dayPrices.get(d).getOpen() + " close: " + dayPrices.get(d).getClose());
+		
+		 }
 	}
 
 	/* returns a string of the split or null if not */
 	public static String calcSplitDay(Double C, Double O) {
-		// System.out.println("close = " + C + " open = " + O);
-		double trysplit = Math.abs(C / O - 3.0);
-		// System.out.println("trySplit: " + trysplit);
 		if (Math.abs(C / O - 2.0) < 0.20) {
-			// System.out.println("split! ");
 			return "2:1";
 		} else if (Math.abs(C / O - 3.0) < 0.30) {
-			// System.out.println("split!\n");
 			return "3:1";
 		} else if (Math.abs(C / O - 1.5) < 0.15) {
-			// System.out.println("split!\n");
 			return "3:2";
 		} else {
 			return null;
